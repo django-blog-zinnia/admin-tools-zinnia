91f91126a5a50b4cb6455bf1bf2ba08eb27ffa07
Fantomas42 <fantomas42@gmail.com>
2012-07-18 12:46:16 +0200

    adding setup script for installation

---
+++ setup.py
@@ -0,0 +1,32 @@
+"""Setup script for admin-tools-zinnia"""
+from setuptools import setup
+from setuptools import find_packages
+
+import admin_tools_zinnia
+
+setup(name='admin_tools_zinnia',
+      version=admin_tools_zinnia.__version__,
+
+      description='Admin tools for django-blog-zinnia',
+      long_description=open('README.rst').read(),
+
+      keywords='django, blog, weblog, zinnia, admin, dashboard',
+
+      author=admin_tools_zinnia.__author__,
+      author_email=admin_tools_zinnia.__email__,
+      url=admin_tools_zinnia.__url__,
+
+      packages=find_packages(exclude=['demo_admin_tools_zinnia']),
+      classifiers=[
+          'Framework :: Django',
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Programming Language :: Python',
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'License :: OSI Approved :: BSD License',
+          'Topic :: Software Development :: Libraries :: Python Modules',],
+
+      license=admin_tools_zinnia.__license__,
+      include_package_data=True,
+      zip_safe=False)
