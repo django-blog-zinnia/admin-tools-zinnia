7d4fc213976ec5cf29f1aa5cd9f6c4d841ded6e4
Fantomas42 <fantomas42@gmail.com>
2014-03-04 13:10:56 +0100

    Re-indent the setup script and add Python 3 classifier

--- setup.py
+++ setup.py
@@ -4,29 +4,32 @@ from setuptools import find_packages
 
 import admin_tools_zinnia
 
-setup(name='admin_tools_zinnia',
-      version=admin_tools_zinnia.__version__,
+setup(
+    name='admin_tools_zinnia',
+    version=admin_tools_zinnia.__version__,
 
-      description='Admin tools for django-blog-zinnia',
-      long_description=open('README.rst').read(),
+    description='Admin tools for django-blog-zinnia',
+    long_description=open('README.rst').read(),
 
-      keywords='django, blog, weblog, zinnia, admin, dashboard',
+    keywords='django, blog, weblog, zinnia, admin, dashboard',
 
-      author=admin_tools_zinnia.__author__,
-      author_email=admin_tools_zinnia.__email__,
-      url=admin_tools_zinnia.__url__,
+    author=admin_tools_zinnia.__author__,
+    author_email=admin_tools_zinnia.__email__,
+    url=admin_tools_zinnia.__url__,
 
-      packages=find_packages(exclude=['demo_admin_tools_zinnia']),
-      classifiers=[
-          'Framework :: Django',
-          'Development Status :: 5 - Production/Stable',
-          'Environment :: Web Environment',
-          'Programming Language :: Python',
-          'Intended Audience :: Developers',
-          'Operating System :: OS Independent',
-          'License :: OSI Approved :: BSD License',
-          'Topic :: Software Development :: Libraries :: Python Modules',],
+    packages=find_packages(exclude=['demo_admin_tools_zinnia']),
+    classifiers=[
+        'Framework :: Django',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Software Development :: Libraries :: Python Modules'],
 
-      license=admin_tools_zinnia.__license__,
-      include_package_data=True,
-      zip_safe=False)
+    license=admin_tools_zinnia.__license__,
+    include_package_data=True,
+    zip_safe=False
+)
